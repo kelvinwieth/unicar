23bfda8bf59af39778d6e6ee257cd8677a7e7b76
Kelvin Wieth <79933699+kelvinwieth@users.noreply.github.com>
2022-07-23 02:41:20 -0300

    add customer bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:unicar/cubits/cars_cubit.dart';
+import 'package:unicar/cubits/customers/customers_cubit.dart';
 import 'package:unicar/pages/home_page.dart';
 import 'package:unicar/repositories/car_repository.dart';
+import 'package:unicar/repositories/customer_repository.dart';
 
 void main() {
   Intl.defaultLocale = 'pt_BR';
@@ -15,13 +17,22 @@ class Unicar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final repository = CarInMemoryRepository();
+    final carsRepository = CarInMemoryRepository();
+    final customersRepository = CustomerInMemoryRepository();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
-        '/home': (context) => BlocProvider(
-              create: (context) => CarsCubit(repository),
+        '/home': (context) => MultiBlocProvider(
+              providers: [
+                BlocProvider<CarsCubit>(
+                  create: (_) => CarsCubit(carsRepository),
+                ),
+                BlocProvider<CustomersCubit>(
+                  create: (_) => CustomersCubit(customersRepository),
+                ),
+              ],
               child: const HomePage(),
             ),
       },
