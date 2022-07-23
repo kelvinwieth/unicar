7d4ea5be0d9b27029bc31c5a2ebea36b84d9ecaf
Kelvin Wieth <79933699+kelvinwieth@users.noreply.github.com>
2022-07-23 16:52:11 -0300

    fix card

--- lib/features/sales/widgets/sale_card.dart
+++ lib/features/sales/widgets/sale_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:unicar/features/sales/cubit/sales_cubit.dart';
 import 'package:unicar/features/sales/models/sale.dart';
 
 class SaleCard extends StatelessWidget {
@@ -11,9 +13,51 @@ class SaleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(sale.car),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+      child: GestureDetector(
+        onTap: () {
+          final cubit = BlocProvider.of<SalesCubit>(context);
+          cubit.startEdition(sale);
+        },
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black,
+                  Colors.blueGrey,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: ListTile(
+              title: Text(
+                sale.car,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    child: const Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                    onTap: () {
+                      final cubit = BlocProvider.of<SalesCubit>(context);
+                      cubit.removeSale(sale);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/features/sales/widgets/sales_view.dart
+++ lib/features/sales/widgets/sales_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:unicar/features/sales/cubit/sales_cubit.dart';
+import 'package:unicar/features/sales/cubit/sales_state.dart';
+import 'package:unicar/features/sales/widgets/sale_card.dart';
 
 class SalesView extends StatelessWidget {
   const SalesView({
@@ -7,8 +11,22 @@ class SalesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Aoba'),
+    return BlocBuilder<SalesCubit, SalesState>(
+      builder: (context, state) {
+        if (state is SalesLoadedState) {
+          return ListView.builder(
+            itemCount: state.sales.length,
+            itemBuilder: (context, index) {
+              final sale = state.sales[index];
+              return SaleCard(sale: sale);
+            },
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
